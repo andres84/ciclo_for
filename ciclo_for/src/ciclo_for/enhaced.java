87a12d8f8fe4d66ca0a437dc7b8937d77f9a1591
sara <sara@localhost>
2016-09-03 18:02:27 -0500

    practica 3 ejercicios

---
+++ ciclo_for/src/ciclo_for/enhaced.java
@@ -0,0 +1,25 @@
+package ciclo_for;
+
+
+
+public class enhaced {
+
+    public static void main(String... andres){
+        
+                String[] lista = new String[3];
+                
+                lista[0] = "andres";
+                lista[1] = "sara";
+                lista[2] = "isabella";
+                
+                for(String dato : lista){
+                    
+                    System.out.println(dato);
+                }
+           
+        
+        
+    }
+
+    
+}
